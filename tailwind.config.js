86ad0da665c8c854e4d8d6c304aa4eb9d4e91a80
ahnafi <nafxd9896@gmail.com>
2023-02-22 16:43:05 +0700

    membuat web portfolio dengan tailwindcss bersama pak Sandhika Galih

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['public/index.html'],
+  theme: {
+    container:{
+      center: true,
+      padding: '16px'
+    },
+    extend: {
+      colors: {
+        primary : '#6b4c35',
+        dark : '#0f172a',
+        secondary: '#64748b',
+        facebook: '#1877F2',
+        youtube: '#FF0000',
+        instagram: '#E4405F'
+      },
+      animation:{
+        'spin-slow': 'spin 10s linear infinite',
+      },
+      screens:{
+        '2xl' : '1320px'
+      }
+
+    },
+  },
+  plugins: [],
+}
